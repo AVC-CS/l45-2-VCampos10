3beb86dcf22c938aac5c1d80d4509eacb9e925c9
VCampos10 <VC195063@icloud.com>
2024-10-11 00:39:43 -0700

    test fail

--- main.py
+++ main.py
@@ -2,21 +2,19 @@ import random
 
 
 def main():
-    total = 0
-    numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+ numbers = []
+ total = 0
+ for i in range (5):
+        numbers.append(random.randint(0,100))
+        print (numbers[i], end = ' ')
+        total += numbers[i]
+ print(total)
+    
+ print(f'The random values are {numbers}')
+ print(f'The Total is {total}')
 
-    print(f'The random values are {numbers}')
-    print(f'The total is {total}')
-
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    return numbers, total
+ return numbers, total
 
 
 if __name__ == '__main__':
