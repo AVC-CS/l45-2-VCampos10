23616c9d4831b9cb57607982efd3be2540d94efd
VCampos10 <VC195063@icloud.com>
2024-10-12 23:44:24 -0500

    update

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def main():
     
  numbers = []
  total = 0
- for i in range (5):
-        numbers.append(random.randint(0,100))
-        print (numbers[i], end = ' ')
-        total += numbers[i]
+ for i in range(5):
+       numbers.append(random.randint(0,100))
+       print (numbers[i], end = ' ')
+       total += numbers[i]
  print(total)
     
  print(f'The random values are {numbers}')
